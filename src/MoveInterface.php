2676c15196b3e584d5fc76aadac68c11f361012e
stavarengo <faelsta@gmail.com>
2019-04-10 14:39:42 -0300

    Add MoveInterface.php

---
+++ src/MoveInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TicTacToe;
+
+interface MoveInterface
+{
+    /**
+     * Makes a move using the actual game board state, against the player.
+     *
+     * $boardState contains a 2D array of the 3x3 board with the 3 possible values:
+     * - X and O represents the player or the bot, as defined by $playerUnit
+     * - empty string means that the field is not yet taken
+     * Example:
+     * [['X', 'O', '']
+     * ['X', 'O', 'O']
+     * [ '', '', '']]
+     *
+     * Returns an array containing X and Y coordinates for the next move
+     * and the unit that should occupy it.
+     * Example: [2, 0, 'O'] - upper right corner with O unit
+     *
+     * @param array $boardState
+     * @param string $playerUnit
+     *
+     * @return array
+     */
+    public function makeMove(array $boardState, string $playerUnit = 'X'): array;
+}
