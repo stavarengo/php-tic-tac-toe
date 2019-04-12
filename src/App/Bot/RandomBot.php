3b1cef63786ca45ab2c4f5c279bcf142186713cd
stavarengo <faelsta@gmail.com>
2019-04-12 20:50:54 -0300

    Create \TicTacToe\App\Bot\RandomBot (implementation of the MoveInterface)

---
+++ src/App/Bot/RandomBot.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+
+namespace TicTacToe\App\Bot;
+
+use TicTacToe\App\Board\Board;
+use TicTacToe\App\FinalResultChecker;
+
+/**
+ * This not choose its next move randomly.
+ */
+class RandomBot implements \MoveInterface
+{
+    /**
+     * @var FinalResultChecker
+     */
+    protected $finalResultChecker;
+
+    /**
+     * DummyBot constructor.
+     */
+    public function __construct()
+    {
+        $this->finalResultChecker = new FinalResultChecker();
+    }
+
+    public function makeMove(array $boardState, string $playerUnit = 'X'): array
+    {
+        if (!Board::isValidUnit($playerUnit)) {
+            return [-1, -1, null];
+        }
+
+        $botUnit = Board::VALID_UNITS[0];
+        if ($playerUnit == $botUnit) {
+            $botUnit = Board::VALID_UNITS[1];
+        }
+
+        if (!Board::isValidBoard($boardState)) {
+            return [-1, -1, null];
+        }
+
+        if ($this->finalResultChecker->getFinalResultFromBoardArray($boardState)) {
+            return [-1, -1, null];
+        }
+
+        $allPossibleMoves = [];
+        foreach ($boardState as $rowIndex => $row) {
+            foreach ($row as $colIndex => $col) {
+                if (!$col) {
+                    $allPossibleMoves[] = [$rowIndex, $colIndex];
+                }
+            }
+        }
+
+        $chosenMove = $allPossibleMoves[array_rand($allPossibleMoves)];
+        $chosenMove[] = $botUnit;
+
+        return $chosenMove;
+    }
+}
--- test/App/Bot/MoveInterfaceTest.php
+++ test/App/Bot/MoveInterfaceTest.php
@@ -7,6 +7,7 @@ namespace TicTacToe\Test\App\Bot;
 use PHPUnit\Framework\TestCase;
 use TicTacToe\App\Board\Board;
 use TicTacToe\App\Bot\DummyBot;
+use TicTacToe\App\Bot\RandomBot;
 
 class MoveInterfaceTest extends TestCase
 {
@@ -19,6 +20,7 @@ class MoveInterfaceTest extends TestCase
     {
         $this->allBotsImplementation = [
             new DummyBot(),
+            new RandomBot(),
         ];
     }
 
