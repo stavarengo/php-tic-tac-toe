c9938aaa9b89a47266ab55b6f9f717b728a2370a
stavarengo <faelsta@gmail.com>
2019-04-12 19:24:17 -0300

    Create \TicTacToe\App\FinalResultChecker

---
+++ src/App/FinalResultChecker.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+
+namespace TicTacToe\App;
+
+
+use TicTacToe\App\Board\Board;
+
+class FinalResultChecker
+{
+    public const DRAW = 'draw';
+
+    public function getFinalResultFromBoardArray(array $board): ?string
+    {
+        // Check if the board is in the valid format
+        if (!isset($board[0][0]) || !isset($board[0][1]) || !isset($board[0][2])
+            || !isset($board[1][0]) || !isset($board[1][1]) || !isset($board[1][2])
+            || !isset($board[2][0]) || !isset($board[2][1]) || !isset($board[2][2])
+        ) {
+            return null;
+        }
+
+        // Checking for Rows for X or O victory.
+        for ($row = 0; $row < 3; $row++) {
+            if ($board[$row][0] && $board[$row][0] == $board[$row][1] && $board[$row][1] == $board[$row][2]) {
+                return $board[$row][0];
+            }
+        }
+
+        // Checking for Columns for X or O victory.
+        for ($col = 0; $col < 3; $col++) {
+            if ($board[0][$col] && $board[0][$col] == $board[1][$col] && $board[1][$col] == $board[2][$col]) {
+                return $board[0][$col];
+            }
+        }
+
+        // Checking for Diagonals for X or O victory.
+        if ($board[0][0] && $board[0][0] == $board[1][1] && $board[1][1] == $board[2][2]) {
+            return $board[0][0];
+        }
+        if ($board[0][2] && $board[0][2] == $board[1][1] && $board[1][1] == $board[2][0]) {
+            return $board[0][2];
+        }
+
+        foreach ($board as $rowIndex => $row) {
+            foreach ($row as $colIndex => $col) {
+                if (!$col) {
+                    // There still have empty places
+                    return null;
+                }
+            }
+        }
+
+        return self::DRAW;
+    }
+
+    public function getFinalResult(Board $board): ?string
+    {
+        return $this->getFinalResultFromBoardArray($board->toArray());
+    }
+
+    public function isDrawFromBoardArray(array $board): bool
+    {
+        return $this->getFinalResultFromBoardArray($board) === self::DRAW;
+    }
+
+    public function isDraw(Board $board): bool
+    {
+        return $this->isDrawFromBoardArray($board->toArray());
+    }
+}
---
+++ test/App/FinalResultCheckerTest.php
@@ -0,0 +1,142 @@
+<?php
+/** @noinspection PhpUnhandledExceptionInspection */
+declare(strict_types=1);
+
+namespace TicTacToe\Test\App;
+
+use PHPUnit\Framework\TestCase;
+use TicTacToe\App\Board\Board;
+use TicTacToe\App\FinalResultChecker;
+
+class FinalResultCheckerTest extends TestCase
+{
+    public function testTryingToGetFinalResultFromAnInvalidBoard()
+    {
+        $invalidBoards = [
+            [],
+            [1, 3, 4],
+            [[], [], []],
+            [[1, 2, 3], [], []],
+            [['', ''], ['', '', ''], ['', '']],
+            [['', '', ''], ['', '', ''], ['', '']],
+        ];
+
+        $finalResultChecker = new FinalResultChecker();
+
+        foreach ($invalidBoards as $invalidBoard) {
+            $this->assertNull(
+                $finalResultChecker->getFinalResultFromBoardArray($invalidBoard),
+                sprintf('Wrong result when using this invalid board "%s".', json_encode($invalidBoard))
+            );
+            $this->assertFalse(
+                $finalResultChecker->isDrawFromBoardArray($invalidBoard),
+                sprintf('Wrong result when using this invalid board "%s".', json_encode($invalidBoard))
+            );
+        }
+    }
+
+    public function testGettingTheWinnerInAllPossibleCombinations()
+    {
+        $unit1 = 'O';
+        $unit2 = 'X';
+        $finalResultChecker = new FinalResultChecker();
+        $allCombinations = [
+            // Rows victory
+            [[0, 0], [0, 1], [0, 2]],
+            [[1, 0], [1, 1], [1, 2]],
+            [[2, 0], [2, 1], [2, 2]],
+
+            // Columns victory
+            [[0, 0], [1, 0], [2, 0]],
+            [[0, 1], [1, 1], [2, 1]],
+            [[0, 2], [1, 2], [2, 2]],
+
+            // Diagonals victory
+            [[0, 0], [1, 1], [2, 2]],
+            [[2, 0], [1, 1], [0, 2]],
+        ];
+
+        foreach ([$unit1, $unit2] as $winnerUnit) {
+            foreach ($allCombinations as $coordinates) {
+                $board = new Board($unit1, $unit2);
+                foreach ($coordinates as $coordinate) {
+                    $board->set($coordinate[0], $coordinate[1], $winnerUnit);
+                }
+
+                $this->assertEquals(
+                    $winnerUnit,
+                    $finalResultChecker->getFinalResult($board),
+                    sprintf('Wrong result when the winner should be "%s", using coordinates "%s".', $winnerUnit, json_encode($coordinates))
+                );
+                $this->assertEquals(
+                    $winnerUnit,
+                    $finalResultChecker->getFinalResultFromBoardArray($board->toArray()),
+                    sprintf('Wrong result when the winner should be "%s", using coordinates "%s".', $winnerUnit, json_encode($coordinates))
+                );
+
+                $this->assertFalse(
+                    $finalResultChecker->isDraw($board),
+                    sprintf('Wrong result when the winner should be "%s", using coordinates "%s".', $winnerUnit, json_encode($coordinates))
+                );
+                $this->assertFalse(
+                    $finalResultChecker->isDrawFromBoardArray($board->toArray()),
+                    sprintf('Wrong result when the winner should be "%s", using coordinates "%s".', $winnerUnit, json_encode($coordinates))
+                );
+            }
+        }
+    }
+
+    public function testTheGameIsDraw()
+    {
+        $board = new Board();
+        $board->set(0, 0, $board->getBotUnit());
+        $board->set(0, 1, $board->getHumanUnit());
+        $board->set(0, 2, $board->getBotUnit());
+        $board->set(1, 0, $board->getHumanUnit());
+        $board->set(1, 2, $board->getBotUnit());
+        $board->set(1, 1, $board->getHumanUnit());
+        $board->set(2, 1, $board->getBotUnit());
+        $board->set(2, 2, $board->getHumanUnit());
+        $board->set(2, 0, $board->getBotUnit());
+
+        $finalResultChecker = new FinalResultChecker();
+
+        $this->assertTrue($finalResultChecker->isDraw($board));
+        $this->assertTrue($finalResultChecker->isDrawFromBoardArray($board->toArray()));
+        $this->assertEquals(FinalResultChecker::DRAW, $finalResultChecker->getFinalResult($board));
+        $this->assertEquals(FinalResultChecker::DRAW, $finalResultChecker->getFinalResultFromBoardArray($board->toArray()));
+    }
+
+    public function testBoardIsEmpty()
+    {
+        $board = new Board();
+        $finalResultChecker = new FinalResultChecker();
+
+        $this->assertFalse($finalResultChecker->isDraw($board));
+        $this->assertFalse($finalResultChecker->isDrawFromBoardArray($board->toArray()));
+        $this->assertNull($finalResultChecker->getFinalResult($board));
+        $this->assertNull($finalResultChecker->getFinalResultFromBoardArray($board->toArray()));
+    }
+
+    public function testBoardIsNotEmptyButTheGameIsNotDoneYet()
+    {
+        $finalResultChecker = new FinalResultChecker();
+
+        // Just one player has played
+        $board = new Board();
+        $board->set(0, 0, $board->getBotUnit());
+        $this->assertFalse($finalResultChecker->isDraw($board));
+        $this->assertFalse($finalResultChecker->isDrawFromBoardArray($board->toArray()));
+        $this->assertNull($finalResultChecker->getFinalResult($board));
+        $this->assertNull($finalResultChecker->getFinalResultFromBoardArray($board->toArray()));
+
+        // Both players has played
+        $board = new Board();
+        $board->set(0, 0, $board->getBotUnit());
+        $board->set(0, 1, $board->getHumanUnit());
+        $this->assertFalse($finalResultChecker->isDraw($board));
+        $this->assertFalse($finalResultChecker->isDrawFromBoardArray($board->toArray()));
+        $this->assertNull($finalResultChecker->getFinalResult($board));
+        $this->assertNull($finalResultChecker->getFinalResultFromBoardArray($board->toArray()));
+    }
+}
